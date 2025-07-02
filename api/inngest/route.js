a78535ca4e0887271fb1bd6977d9b5808b8f0971
Harsh <hksharma2004@gmail.com>
2025-07-02 23:26:28 +0530

    fix route(2)

--- api/inngest/route.js
+++ api/inngest/route.js
@@ -1,10 +1,13 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
+import { inngest } from "@/config/inngest";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
   ],
 });
